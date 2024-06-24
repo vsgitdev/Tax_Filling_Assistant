2128fba4a53ce9b4142d5a67468c0e9a4f8239fe
vsgitdev <vasiliki.tsiotinou@gmail.com>
2024-06-24 18:39:31 +0300

    Database correction

--- database_setup.py
+++ database_setup.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -7,8 +7,8 @@ Base = declarative_base()
 class TaxInfo(Base):
     __tablename__ = 'tax_info'
     id = Column(Integer, primary_key=True)
-    income = Column(String, nullable=False)
-    expenses = Column(String, nullable=False)
+    income = Column(Float, nullable=False)
+    expenses = Column(Float, nullable=False)
 
 engine = create_engine('sqlite:///tax_info.db')
 Base.metadata.create_all(engine)
